b7a3e8d780b90c5c0f26e6c9d6725c718da61480
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2024-03-24 16:48:06 -0300

    99%

--- script.js
+++ script.js
@@ -9,11 +9,22 @@ menuButton.addEventListener("click", () => {
 
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
-    items:2,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true
+    items: 2,
+    loop: true,
+    margin: 10,
+    autoplay: false, // Desativa o autoplay inicialmente
+    autoplayHoverPause: true,
+    dots: true, // Adiciona os pontos indicadores abaixo do carrossel
+    responsive: {
+      0: {
+        items: 1,
+        autoplay: true // Ativa o autoplay em dispositivos móveis
+      },
+      768: {
+        items: 2,
+        nav: true, // Adiciona as setas de navegação em desktop
+        autoplay: false // Desativa o autoplay em desktop
+      }
+    }
   });
 });
